b63c0d8a34d5071e9677c5661177b21870364cca
Midani Rachdi <rachdi.midani@gmail.com>
2018-07-29 18:12:23 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require("path"),
+      HtmlWebpackPlugin = require("html-webpack-plugin"),
+      CleanWebpackPlugin = require("clean-webpack-plugin"),
+      MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+      Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js" // serves a bundle from memory (not minified + has devServer code too)
+    // publicPath : '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["env"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader" ]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]", // keep the old name + extension instead of the hash
+              outputPath: "img/" //copy here
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "fonts/"
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new Dotenv(),
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  devServer: {
+    open: true,
+    port: 8080,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  },
+  performance: {
+    hints: false
+  }
+};
